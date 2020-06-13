c4fbebb4ca2f824f083a20144b125ae8b763cc35
Felipe Tonietto <61717379+felipetonietto@users.noreply.github.com>
2020-06-12 22:06:07 -0300

    Add files via upload

---
+++ build/precache-manifest.43318474f2e1e04a793002feb4f6bf04.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbcbc6b25d806be4448e83a530a2bc3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ad06bb5f31f0814c059",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "655eacc9c26d22dceae1",
+    "url": "/static/css/main.e9636603.chunk.css"
+  },
+  {
+    "revision": "1ad06bb5f31f0814c059",
+    "url": "/static/js/2.d29f1efc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.d29f1efc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "655eacc9c26d22dceae1",
+    "url": "/static/js/main.9ba0728e.chunk.js"
+  },
+  {
+    "revision": "acbc1148123d4ef1aa29",
+    "url": "/static/js/runtime-main.6a0825b5.js"
+  },
+  {
+    "revision": "714a74e306b458dd9dab06acfbf699c3",
+    "url": "/static/media/blockchain.714a74e3.svg"
+  },
+  {
+    "revision": "a8a7f6d921d41864bd2a881e9766777b",
+    "url": "/static/media/exemplopagina.a8a7f6d9.png"
+  },
+  {
+    "revision": "dbcb7dc735500fbfbe19d4d22e66277c",
+    "url": "/static/media/mining.dbcb7dc7.svg"
+  },
+  {
+    "revision": "4e85062eeb47a8233cc266b74c92439b",
+    "url": "/static/media/novousuario.4e85062e.svg"
+  },
+  {
+    "revision": "0b3c28634aec013105d7f39594572689",
+    "url": "/static/media/paper.0b3c2863.svg"
+  },
+  {
+    "revision": "9abe5f92082aba665e6a1285a7ca0a9e",
+    "url": "/static/media/privatekeys.9abe5f92.svg"
+  }
+]);
